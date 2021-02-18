895982b5fcc2b2fab59bf7aa79618c26aec34ad2
Leonardo <mr_kunkel@hotmail.com>
2021-02-18 08:54:53 -0600

    first commit

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+
+var test = 'Test!';
+console.log(test);
---
+++ src/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+
+const app = express();
+
+// Routes
+import IndexRoutes from '../routes/index.routes';
+import TaskRoutes from '../routes/task.routes';
+
+// Settings
+app.set('port', process.env.PORT || 3000);
+
+//Middlewares
+app.use(express.json());
+
+app.use(IndexRoutes);
+app.use('/tasks',TaskRoutes);
+
+export default app;
